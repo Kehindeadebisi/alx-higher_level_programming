b841affbb5dd15a370a9e27e0d17cdde1999503b
Kehindeadebisi <Kehindeadebisi95@gmail.com>
2021-11-16 10:24:13 -0800

    fixed elif errors

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-last = number % 10
+
+if number >+ 0:
+    last = number % 10
+elif number < 0:
+    last = ((number * -1) % 10) * -1
+
 ist = "Last digit of"
 if last < 6 and last != 0:
     print("{} {} is {} and is less than 6 and not 0".format(ist, number, last))
