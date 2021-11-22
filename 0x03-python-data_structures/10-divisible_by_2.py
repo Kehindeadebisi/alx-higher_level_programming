3dac70af81c31a1401cc91a3ac73a09cc75eacfa
Kehindeadebisi <Kehindeadebisi95@gmail.com>
2021-11-22 05:42:39 -0800

    fixed errors

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
     new_list = []
-    if my_list == None:
+    if my_list is None:
         return None
     for i in my_list:
         if (i % 2 == 0):
--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    max = my_list[0]
+    big = my_list[0]
     if len(my_list) == 0:
         return None
     else:
         for i in my_list:
-            if i > max:
-                max = i
-                return max
+            if i > big:
+                big = i
+                return big
