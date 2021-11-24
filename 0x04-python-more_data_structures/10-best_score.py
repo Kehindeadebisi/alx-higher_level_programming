cb50609154879d9e1e1a5c5788090f7c6ff41c5c
Kehindeadebisi <Kehindeadebisi95@gmail.com>
2021-11-24 11:31:23 -0800

    fixed errors

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,5 +2,7 @@
 def best_score(a_dictionary):
     if a_dictionary is None:
         return None
-    else:
-        return max(a_dictionary)
+    max_score = max(a_dictionary.values(), default=None)
+    for x, y in a_dictionary.items():
+        if y == max_score:
+            return x
