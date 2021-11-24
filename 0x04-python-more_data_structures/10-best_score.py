5afbdffdc897fbf54ed0f814877e4470e79e6c05
Kehindeadebisi <Kehindeadebisi95@gmail.com>
2021-11-24 11:40:58 -0800

    fixed error

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,7 +2,12 @@
 def best_score(a_dictionary):
     if a_dictionary is None:
         return None
-    max_score = max(a_dictionary.values(), default=None)
-    for x, y in a_dictionary.items():
-        if y == max_score:
-            return x
+    score = list(a_dictionary.keys())[0]
+    initial = a_dictionary[score]
+    for key, value in a_dictionary.items():
+        if value > initial:
+            score = key
+            initial = value
+    if initial <= 0:
+        return None
+    return score
--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -4,12 +4,14 @@ def roman_to_int(roman_string):
         return 0
     roman_dict = {"I": 1, "V": 5, "X": 10, "L": 50,
                   "C": 100, "D": 500, "M": 1000}
-    number = roman_dict[roman_string[0]]
-    for i in range(len(roman_string) - 1):
-        current = roman_dict[roman_string[i]]
-        nxt = roman_dict[roman_string[i + 1]]
-        if nxt <= current:
-            number += nxt
+    rev = roman_string[::-1]
+    number = roman_dict[rev[0]]
+
+    for i in range(len(rev) - 1):
+        a = roman_dict[rev[i]]
+        b = roman_dict[rev[i + 1]]
+        if b >= a:
+            number += b
         else:
-            number = nxt - current
+            number -= before
     return number
