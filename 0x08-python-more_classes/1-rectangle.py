6131f04b2cd9ba784d2d69e142b6fc5fbe44640e
Kehindeadebisi <Kehindeadebisi95@gmail.com>
2021-12-13 11:10:29 -0800

    added readme and tasks 0 and 1

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+class Rectangle:
+    def __init__(self, width=0, height=0):
+        self.__width
+    @property
+    def width(self):
+        return self.width
+    @width.setter
+    def width(self, value):
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be an >= 0")
+
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be an >= 0")
+        
+        self.__height = value
